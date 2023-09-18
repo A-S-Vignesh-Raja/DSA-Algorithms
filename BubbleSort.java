6cb1b6b5f41cee3a7ddbe6e13d33da8af5e6ec86
A.S.Vignesh Raja <136581635+A-S-Vignesh-Raja@users.noreply.github.com>
2023-09-18 20:38:26 +0530

    BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		Bubblesort(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	static void Bubblesort(int[] arr) {
+		for(int i=0;i<arr.length;i++) {
+			for(int j=1;j<arr.length-i;j++) {
+				if(arr[j]<arr[j-1]) {
+					int temp=arr[j];
+					arr[j]=arr[j-1];
+					arr[j-1]=temp;
+				}
+			}
+		}
+	}
+
+}
