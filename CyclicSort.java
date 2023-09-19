247bb3fd070d8fbe94751235e6752bbf30550e1e
A.S.Vignesh Raja <136581635+A-S-Vignesh-Raja@users.noreply.github.com>
2023-09-19 14:49:03 +0530

    CyclicSort.java

---
+++ CyclicSort.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class CyclicSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		Cyclicsort(arr);
+		System.out.println(Arrays.toString(arr));
+		sc.close();
+	}
+	
+	static void Cyclicsort(int[] arr) {
+		int i=0;
+		while(i<arr.length) {
+			int correctindex=arr[i]-1;
+			if(arr[i]!=arr[correctindex]) {
+				swap(arr,i,correctindex);
+			}
+			else {
+				i++;
+			}
+		}
+	}
+	static void swap(int[] arr,int first, int last) {
+		int temp=arr[first];
+		arr[first]=arr[last];
+		arr[last]=temp;
+	}
+
+}
